768b36a49929b413e0ab96e9794187f029e301f6
Dano Williams <dan.o.williams@gmail.com>
2016-01-19 13:39:12 -0800

    ran initial build

--- builder/list_item_hunter.js
+++ builder/list_item_hunter.js
@@ -1,12 +1,12 @@
-/* 
- * patternlab-node - v1.0.0 - 2015 
+/** 
+ * patternlab-node - v1.0.0 - 2016
  * 
  * Brian Muenzenmeyer, and the web community.
- * Licensed under the MIT license. 
+ * Licensed under the MIT license.
  * 
- * Many thanks to Brad Frost and Dave Olsen for inspiration, encouragement, and advice. 
- *
- */
+ * Many thanks to Brad Frost and Dave Olsen for inspiration, encouragement, and advice.
+ * 
+ **/
 
 (function () {
 	"use strict";
